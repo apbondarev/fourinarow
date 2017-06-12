fb35f34c7e36c8f63fb4e752c76671e7f93642d7
apbondarev <apbondarev@gmail.com>
2017-06-12 21:48:15 +0300

    disk falling down

--- public/fourinarow.js
+++ public/fourinarow.js
@@ -12,6 +12,9 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Four in a row',
   });
 
 var sprites = [];
+var coords = [];
+
+var currentDisk;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -19,30 +22,105 @@ function preload() {
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#fff';
   
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  
+  // Set the world (global) gravity
+  game.physics.arcade.gravity.y = 800;
+
   game.load.image('blue', 'img/blue-circle.png');
   game.load.image('red', 'img/red-circle.png');
+
+  initCoords();
+  initSprites();
 }
 
-function create() {
+function initCoords() {
   var intervalX = Math.floor( (game.width - BUTTON_WIDTH * COLUMN_COUNT) / (COLUMN_COUNT + 1) );
   var intervalY = Math.floor( (game.height - BUTTON_WIDTH * ROW_COUNT) / (ROW_COUNT + 1) );
   var interval = Math.min(intervalX, intervalY);
   
   var y = Math.floor(( game.height - BUTTON_WIDTH * ROW_COUNT - interval * (ROW_COUNT - 1) ) / 2);
   for (var i = 0; i < ROW_COUNT; i++) {
-    sprites[i] = [];
+    coords[i] = [];
     var x = Math.floor(( game.width - BUTTON_WIDTH * COLUMN_COUNT - interval * (COLUMN_COUNT - 1) ) / 2);
     for (var j = 0; j < COLUMN_COUNT; j++) {
-      ball = game.add.sprite(x, y, (i+j)%2==0 ? 'blue': 'red');
+      coords[i][j] = {x: x, y: y};
       x += interval + BUTTON_WIDTH;
     }
     y += interval + BUTTON_WIDTH;
   }
 }
 
+function initSprites() {
+  for (var i = 0; i < ROW_COUNT; i++) {
+    sprites[i] = [];
+  }
+}
+
+function createDisk(i, j, color) {
+  var c = coords[i][j];
+
+  var disk = game.add.sprite(c.x, c.y, color);
+  game.physics.enable(disk, Phaser.Physics.ARCADE);
+  
+  // This makes the game world bounce-able
+  disk.body.collideWorldBounds = true;
+  disk.body.checkCollision.up = false;
+  disk.body.checkCollision.down = false;
+  
+  // This sets the image bounce energy for the horizontal  and vertical vectors (as an x,y point). "1" is 100% energy return
+  disk.body.bounce.set(0.1);
+  
+  // Enables all kind of input actions on this image (click, etc)
+  disk.inputEnabled = true;
+  disk.input.enableDrag();
+
+  // Input event listeners
+  disk.events.onInputDown.add(inputDownListener, disk);
+  disk.events.onInputUp.add(inputUpListener, disk);
+  disk.events.onDragStart.add(startDrag, this);
+  disk.events.onDragStop.add(stopDrag, this);
+
+  sprites[i][j] = disk;
+  return disk;
+}
+
+function startDrag(sprite) {
+  //  You can't have a sprite being moved by physics AND input, so we disable the physics while being dragged
+  sprite.body.moves = false;
+}
+
+function stopDrag(sprite) {
+  //  And re-enable it upon release
+  sprite.body.moves = true;
+}
+
+function inputDownListener() {
+  currentDisk = this;
+  
+  // remember coordinates where input holds the disk
+  currentDisk.holdX = game.input.activePointer.x;
+  currentDisk.holdY = game.input.activePointer.y;
+}
+
+function inputUpListener() {
+  currentDisk = null;
+}
+
+function create() {
+  for (var i = 0; i < 1; i++) {
+    for (var j = 0; j < COLUMN_COUNT; j++) {
+      var color = (i+j) % 2 == 0 ? 'blue': 'red';
+      createDisk(i, j, color);
+    }
+  }
+}
+
 function update() {
-//  circle.x = game.input.activePointer.x;
-//  circle.y = game.input.activePointer.y;
+  if (currentDisk) {
+//    currentDisk.x = game.input.activePointer.x;
+//    currentDisk.y = game.input.activePointer.y;
+  }
 }
 
 function render() {
